b3d1e8066588675040f57861e09f82ef314ef876
Griotold <gotjd9773@naver.com>
2022-11-20 12:20:09 +0900

    주문 도메인 개발 - 주문 서비스 개발

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -1,7 +1,9 @@
 package jpabook.jpashop.domain;
 
 import jpabook.jpashop.domain.item.Item;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
@@ -11,6 +13,7 @@ import static javax.persistence.FetchType.LAZY;
 @Entity
 @Table(name = "order_item") // 안해줘도 알아서 스네이크로 만들어주긴 함
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OrderItem {
 
     @Id @GeneratedValue
@@ -28,8 +31,9 @@ public class OrderItem {
     private int orderPrice; // 주문 가격
     private int count; // 주문 수량
 
+//    protected  OrderItem() {}
     //==생성 메소드==//
-    public OrderItem createOrderItem(Item item, int orderPrice, int count){
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count){
         OrderItem orderItem = new OrderItem();
         orderItem.setItem(item);
         orderItem.setOrderPrice(orderPrice);
@@ -41,6 +45,9 @@ public class OrderItem {
     }
 
     //==비즈니스 로직==//
+    /**
+     * 취소
+     * */
     public void cancel() {
         getItem().addStock(count);
     }
